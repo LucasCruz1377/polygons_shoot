b50e32a4e569a229b35bc16e0d2e974fb9116016
Lucas pc Isbet <lucascsilva1402@gmail.com>
2026-04-14 08:07:18 -0300

    hiperdash particles

--- fireball.gd
+++ fireball.gd
@@ -4,10 +4,6 @@ const SPEED = 1000
 var dmg = 2
 func _physics_process(delta: float) -> void:
 	position += transform.x * SPEED * delta
-	
-	
-func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
-	queue_free()
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("enemies") or body.is_in_group("parede"):
--- player.gd
+++ player.gd
@@ -1,6 +1,7 @@
 extends CharacterBody2D
 
-@onready var firehole: Marker2D = $Marker2D
+@onready var ponta: Marker2D = $ponta
+@onready var particles: GPUParticles2D = $particles
 
 const BULLET = preload("res://fireball.tscn")
 const acceleration = 200.00
@@ -11,19 +12,19 @@ var cooldown = CD_MAX
 var mouseaim = true
 var hiperdashing = false
 func _process(delta: float) -> void:
-		
+	
 	if cooldown >= 0:
 		cooldown -= 1
 	
 	if Input.is_action_just_pressed("ctrlflip"):
 		mouseaim = !mouseaim
 
-	if mouseaim:
-		look_at(get_global_mouse_position())
-	if !mouseaim:
-		arrowsctrl(delta)
 		
 	if !hiperdashing:
+		if mouseaim:
+			look_at(get_global_mouse_position())
+		if !mouseaim:
+			arrowsctrl(delta)
 		if Input.is_action_pressed("accelerate"):
 			accelerate(delta)
 		if Input.is_action_pressed("brake"):
@@ -44,7 +45,7 @@ func brake(delta:float):
 func fire():
 		var instance_bullet = BULLET.instantiate()
 		get_tree().current_scene.add_child(instance_bullet)
-		instance_bullet.global_position = firehole.global_position
+		instance_bullet.global_position = ponta.global_position
 		instance_bullet.rotation = rotation
 		cooldown = CD_MAX
 
@@ -57,10 +58,12 @@ func hiperdash():
 	hiperdashing = true
 	Engine.time_scale = 0.2
 	await get_tree().create_timer(0.2).timeout
+	particles.emitting = true
 	Engine.time_scale = 1
 	velocity += transform.x * SPEED * 10
 	await get_tree().create_timer(0.15).timeout
 	velocity *= 0.2
 	
 	await get_tree().create_timer(0.3).timeout
+	particles.emitting = false
 	hiperdashing = false
